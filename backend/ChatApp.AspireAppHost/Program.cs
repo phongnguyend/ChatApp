14fe6cbebc218f07624a5c465b7a9a2caaeb9d0e
Phong Nguyen <nguyendoanphong.it@gmail.com>
2026-08-01 17:48:21 +0700

    Aspire App Host

---
+++ backend/ChatApp.AspireAppHost/Program.cs
@@ -0,0 +1,6 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<Projects.ChatApp_Api>("ChatApp-Api");
+var background = builder.AddProject<Projects.ChatApp_Background>("ChatApp-Background");
+
+builder.Build().Run();
