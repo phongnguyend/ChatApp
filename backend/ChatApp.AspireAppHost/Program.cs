21fb820959fd251d23efd619961e4c46f1320ad8
Phong Nguyen <nguyendoanphong.it@gmail.com>
2026-08-02 12:45:04 +0700

    Azure Functions

--- backend/ChatApp.AspireAppHost/Program.cs
+++ backend/ChatApp.AspireAppHost/Program.cs
@@ -1,8 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.ChatApp_Api>("ChatApp-Api");
-var background = builder.AddProject<Projects.ChatApp_Background>("ChatApp-Background");
-
-builder.AddAzureFunctionsProject<Projects.ChatApp_AzureFunctions>("ChatApp-AzureFunctions");
+//var background = builder.AddProject<Projects.ChatApp_Background>("ChatApp-Background");
+builder.AddAzureFunctionsProject<Projects.ChatApp_AzureFunctions>("ChatApp-AzureFunctions").WaitFor(api);
 
 builder.Build().Run();
